c3b1dab50f3a4915f45e1eddc35e67123cc6bda7
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-04-07 10:43:47 +0300

    save files

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.round(Math.random() * 10 + 1);
+const num3 = 100;
+const num4 = 25;
+let mod = num3 % num4;
+Number.MAX_VALUE(11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
